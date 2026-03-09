073b08d1c7a4ec0fbb155a88efb5df9184f1b7e9
AlliahShane <salvoalliahshane@gmail.com>
2026-03-09 10:29:05 +0800

    adding some table

--- Migration/table_schema.sql
+++ Migration/table_schema.sql
@@ -1,5 +1,20 @@
 START TRANSACTION;
 
+-- Table: Encryption and Decryption
+
+CREATE TABLE encrypt_and_decrypt_file (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    log_id INT NULL,
+    rating TINYINT NOT NULL,
+    comment VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (log_id) REFERENCES user_activity_logs(id)
+) ENGINE=InnoDB;
+
+
 -- Table: Encryption Keys 
 
 CREATE TABLE encryption_keys (
