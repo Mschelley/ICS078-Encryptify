def5913efdbeda34cd03771050371f496cc7e4a6
AlliahShane <salvoalliahshane@gmail.com>
2026-03-09 10:50:56 +0800

    inserting values to encrypt and decrypt file

---
+++ Insertion/encrypt_and_decrypt_file.sql
@@ -0,0 +1,6 @@
+INSERT INTO encrypt_and_decrypt_file (user_id, log_id, rating, comment)
+VALUES
+(1, 1, 5, 'Encryption and decryption worked successfully.'),
+(2, 3, 4, 'File was encrypted properly but decryption took time.'),
+(1, 4, 5, 'System worked smoothly during encryption process.'),
+(3, 6, 3, 'Encountered minor delay while decrypting file.');
